4a335f9d7f19eee3100105fb8d20b47a97b4b5ac
laxmimerit <laxmimerit@gmail.com>
2024-10-30 09:24:20 +0530

    html loader

---
+++ 8. Document Loaders/scripts/llm.py
@@ -0,0 +1,37 @@
+from langchain_ollama import ChatOllama
+
+from langchain_core.prompts import (SystemMessagePromptTemplate, HumanMessagePromptTemplate,
+                                    ChatPromptTemplate)
+
+
+
+from langchain_core.output_parsers import StrOutputParser
+
+base_url = "http://localhost:11434"
+model = 'llama3.2:3b'
+
+llm = ChatOllama(base_url=base_url, model=model)
+
+system = SystemMessagePromptTemplate.from_template("""You are helpful AI assistant who answer user question based on the provided context.
+                                                    Do not answer in more than {words} words""")
+
+prompt = """Answer user question based on the provided context ONLY! If you do not know the answer, just say "I don't know".
+            Below context is the text data extracted from the website. You need to think carefully while selecting relevant text.
+            ### Context:
+            {context}
+
+            ### Question:
+            {question}
+
+            ### Answer:"""
+
+prompt = HumanMessagePromptTemplate.from_template(prompt)
+
+messages = [system, prompt]
+template = ChatPromptTemplate(messages)
+
+qna_chain = template | llm | StrOutputParser()
+
+def ask_llama(context, question, words=50):
+    response = qna_chain.invoke({'context': context, 'question': question, 'words': words})
+    return response
